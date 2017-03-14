7db2816f1957ef3d3cf738c08690a522bb71f58f
Thomas Meyer <thomas.meyer@kaizen-solutions.net>
2017-03-14 10:38:10 +0100

    First Commit

---
+++ BMI160Gen.h
@@ -0,0 +1,17 @@
+#ifndef _BMI160GEN_H_
+#define _BMI160GEN_H_
+
+//#define BMI160GEN_USE_CURIEIMU
+#include "CurieIMU.h"
+
+class BMI160GenClass : public CurieIMUClass {
+    protected:
+        int interrupt_pin = -1;
+    public:
+        bool begin(const int spi_cs_pin = -1, const int intr_pin = -1);
+        void attachInterrupt(void (*callback)(void));
+};
+
+//extern BMI160GenClass BMI160;
+
+#endif /* _BMI160GEN_H_ */
